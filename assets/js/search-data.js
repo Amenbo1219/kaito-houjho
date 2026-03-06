02de137b331b2f8950f59cec939260805fbd5a9b
Amenbo1219 <68372605+Amenbo1219@users.noreply.github.com>
2026-03-06 03:07:30 +0000

    Deploying to gh-pages from @ Amenbo1219/kaito-houjho@178254b85ebd8f669e01217469bddba19feb8a36 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,21 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/kaito-houjho/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/kaito-houjho/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/kaito-houjho/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
